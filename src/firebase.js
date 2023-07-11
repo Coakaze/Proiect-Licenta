7595b4a5408965e146de7157d3b26dc97f43fe59
Rotaru Catalin <62445195+Coakaze@users.noreply.github.com>
2023-07-11 19:23:27 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCWuAfqY8t_lq9_OEhA1l3x1cgcyCxtXe0",
-    authDomain: "licenta-f5d5c.firebaseapp.com",
-    projectId: "licenta-f5d5c",
-    storageBucket: "licenta-f5d5c.appspot.com",
-    messagingSenderId: "424812538406",
-    appId: "1:424812538406:web:c7c9caa78748081f26d4e7",
-    measurementId: "G-B13CYKYQDX"
+
 };
 
 // Initialize Firebase
